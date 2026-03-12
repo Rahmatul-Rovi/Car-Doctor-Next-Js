5afdc9116867ce0c759ded78931e120ec39b6950
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-12 12:10:11 +0600

    Navbar Section Updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
+  const navMenu = () => {
+    return (
+       <>
+       <li>
+        <Link href="/">Home</Link>
+       </li>
+       
+        <li>
+        <Link href="/">About</Link>
+       </li>
+
+         <li>
+        <Link href="/">Services</Link>
+       </li>
+
+         <li>
+        <Link href="/">Blog</Link>
+       </li>
+
+         <li>
+        <Link href="/">Contact</Link>
+       </li>
+      </>
+    )
+     
+    
+  }
   return (
     <div>
      <div className="navbar bg-base-100 shadow-sm">
@@ -12,18 +41,12 @@ export default function Navbar() {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       {navMenu()}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <Link href="/" className=" text-xl">
+      <Image src={'/assets/logo.svg'} width={107} height={87}/>
+    </Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
