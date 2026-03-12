9b95af79969226c383538ba425cb940f13b73462
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-12 12:48:15 +0600

    Services Section Updated

--- app/components/ServicesSection.tsx
+++ app/components/ServicesSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 export default async function ServicesSection() {
@@ -7,8 +8,8 @@ export default async function ServicesSection() {
     <div className='grid grid-cols-12'>
       {data.map((item)=> {
       return (
-        <div className='col-span-4'>
-            {JSON.stringify(item)}
+        <div className='col-span-12 md:col-span-6 lg:col-span-4' key={item._id}>
+            <Image src={item.img} width={314} height={208}/>
         </div>
       )
       })}
