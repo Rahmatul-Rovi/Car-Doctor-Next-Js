f351ea33ee09cf6d0dace84b6aff3d5a21f7e7cf
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-12 19:13:32 +0600

    Service Details Section Updated

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -1,9 +1,28 @@
+import dbConnect, { collectionNamesObj } from '@/app/lib/dbConnect';
+import { ObjectId } from 'mongodb';
+import Image from 'next/image';
 import React from 'react'
 
-export default function ServiceDetailspage() {
+export default async function ServiceDetailspage({params}) {
+    const p = await params;
+    const servicesCollection = dbConnect(collectionNamesObj.servicesCollection);
+    const data = await servicesCollection.findOne({_id: new ObjectId(p.id)});
+
   return (
     <div>
-      
+        <section>
+           <figure className='flex justify-center w-full relative'>
+             <Image src={"/assets/images/checkout/checkout.png"} 
+            width={1137}
+            height={300}
+            alt={"Banner"} />
+            <div className='absolute w-full h-full border-2 border-red-400'>
+
+            </div>
+           </figure>
+        </section>
+      <p>{p.id}</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   )
 }
