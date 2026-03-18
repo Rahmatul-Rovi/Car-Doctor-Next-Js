ebbbea8ced37738866915f2b5cd5994f0d6b6433
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-19 05:53:48 +0600

    CheckOut Section Updated

---
+++ app/checkout/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default async function CheckoutPage({params}) {
+      const p = await params;
+    const res = await fetch(`http://localhost:3000/api/services/${p.id}`);
+    const data = await res.json();
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  )
+}
