5933e5bcc71e23118989225ad90a3a20fffcd2bc
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-20 05:23:15 +0600

    Route Section Added

---
+++ app/api/services/route.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GiH2O } from 'react-icons/gi'
+
+export default function route() {
+  return (
+    <div>
+      <h2> This is Home <h2/>
+    </div>
+  )
+}
--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -2,12 +2,16 @@ import CheckoutForm from '@/app/components/forms/CheckoutForm';
 import React from 'react'
 
 export default async function CheckoutPage({params}) {
-      const p = await params;
+    const p = await params;
+    
+    // ১. ডাটা ফেচ করছেন ঠিক আছে
     const res = await fetch(`http://localhost:3000/api/services/${p.id}`);
-    const data = await res.json();
-  return (
-    <div>
-      <CheckoutForm/>
-    </div>
-  )
+    const serviceData = await res.json(); // নামটা 'serviceData' দিলাম যাতে বুঝতে সুবিধা হয়
+
+    return (
+        <div>
+            {/* ২. এখানে 'data' প্রপস হিসেবে ডাটা-টা পাঠাতে হবে */}
+            <CheckoutForm data={serviceData} />
+        </div>
+    )
 }
