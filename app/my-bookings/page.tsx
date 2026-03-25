4b5f8abaca0c0980245735a7c3f57f4ee8ce841c
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-25 15:24:22 +0600

    Bookings Section Updated

---
+++ app/my-bookings/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyBookingsTable from '../register/components/tables/MyBookingsTable'
+
+export default function MyBookingsPage() {
+  return (
+    <div>
+      <MyBookingsTable></MyBookingsTable>
+    </div>
+  )
+}
