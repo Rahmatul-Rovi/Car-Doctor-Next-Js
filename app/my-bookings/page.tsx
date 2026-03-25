7558635c66b81d4886d9a97a4680cbf57a159d84
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-25 15:31:55 +0600

    Bookings Section Updated

--- app/my-bookings/page.tsx
+++ app/my-bookings/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import MyBookingsTable from '../register/components/tables/MyBookingsTable'
 
 export default function MyBookingsPage() {
+  const [data, setData] = useState([]);
+  useEffect(()=> {
+    const fetchMyBookings = async () => {
+      const res = await fetch("http://localhost:3000/api/services")
+      const d = await res.json();
+      setData(d);
+    }
+  }, [])
   return (
     <div>
-      <MyBookingsTable></MyBookingsTable>
+      <MyBookingsTable> data={data}</MyBookingsTable>
     </div>
   )
 }
