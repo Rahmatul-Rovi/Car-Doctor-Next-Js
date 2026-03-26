7408f157dbd0626eb6649b0204b06d79c2b87686
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-27 00:18:44 +0600

    route Section Updated

--- app/api/services/[id]/route.ts
+++ app/api/services/[id]/route.ts
@@ -1,12 +1,25 @@
+import { authOptions } from '@/app/lib/authOptions';
 import dbConnect, { collectionNamesObj } from '@/app/lib/dbConnect';
 import { ObjectId } from 'mongodb';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from "next/server";
 
 export const DELETE = async (req, {params}) => {
+ const bookinCollection = dbConnect(collectionNamesObj.bookingCollection);
   const p = await params;
-  const bookinCollection = dbConnect(collectionNamesObj.bookingCollection);
+ 
   const query = {_id: new ObjectId(p.id)}
+
+  //Validation
+  const session = await getServerSession(authOptions);
+  const currentBooking = await bookinCollection.findOne(query)
+
+  //Deleting User Specific Booking
+
+ 
   const deleteResponse = await bookinCollection.deleteOne(query);
+
+  return NextResponse(deleteResponse);
 
 }
 
