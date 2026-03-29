df937d01735445df2cc5f059cfbcbef3a58b482f
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-29 19:15:27 +0600

    Server Section Updated

--- app/api/my-bookings/[id]/route.ts
+++ app/api/my-bookings/[id]/route.ts
@@ -1,8 +1,12 @@
 import dbConnect , {collectionNamesObj} from "@/app/lib/dbConnect";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server"
 
 export const GET = async (req, {params})=>{
 
+    const p = await params;
     const bookingCollection = dbConnect(collectionNamesObj.bookinCollection);
-    return NextResponse.json({});
+    const query = {_id: new ObjectId(p.id)};
+    const singleBooking = await bookingCollection.findOne(query)
+    return NextResponse.json(singleBooking);
 }
