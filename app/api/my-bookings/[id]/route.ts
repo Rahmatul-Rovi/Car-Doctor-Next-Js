c593ea86c18e0e56e7420f1e9e56f06d03e0567c
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-30 09:50:16 +0600

    route Section Updated

--- app/api/my-bookings/[id]/route.ts
+++ app/api/my-bookings/[id]/route.ts
@@ -10,3 +10,20 @@ export const GET = async (req, {params})=>{
     const singleBooking = await bookingCollection.findOne(query)
     return NextResponse.json(singleBooking);
 }
+
+
+export const PATCH = async (requestAnimationFrame, {params}) => {
+      const p = await params;
+    const bookingCollection = dbConnect(collectionNamesObj.bookinCollection);
+    const query = {_id: new ObjectId(p.id)};
+
+    const body = await req.json()
+    const filter = {
+        $set: ...body
+    }
+    const option = {
+        upsert: true;
+    }
+
+    const updateResponse = await bookingCollection.updateOne(query, filter)
+}
